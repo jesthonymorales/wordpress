8b4ecf7fcfce70b857682a3665431dc6d8340952
Jesthony Morales <jesthonymorales@Jesthony-Morales-MBP15.local>
2021-09-07 17:21:12 +0800

    Plugins and Custom Post Type Added

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -636,4 +636,14 @@ function twentytwentyone_add_ie_class() {
 	</script>
 	<?php
 }
-add_action( 'wp_footer', 'twentytwentyone_add_ie_class' );
+
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+ 
+function add_my_post_types_to_query( $query ) {
+
+ 
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'recipes' ) );
+    return $query;
+
+}
--- wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -12,7 +12,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
 	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
 
 	<div class="entry-content">
