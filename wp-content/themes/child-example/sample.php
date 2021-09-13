a2aa6abee45d8b2c5b98acf45e0a17cd97a75e20
Jesthony Morales <jesthonymorales@Jesthony-Morales-MBP15.local>
2021-09-13 17:48:12 +0800

    WP-102-Training Initial Push

---
+++ wp-content/themes/child-example/sample.php
@@ -0,0 +1,12 @@
+<div id="primary">
+	<div id="content" role="main">
+		
+		<?php
+
+			$value = get_field( "text_field" );
+			echo "<br /><br /> > ".$value;
+		?>
+
+
+	</div><!-- #content -->
+</div><!-- #primary -->
